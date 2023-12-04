ac7783a2d8c14409a9a5563b160c21d205444586
Israel-Junqueira <israelribeiro313@gmail.com>
2023-12-04 20:53:49 -0300

    ultimas alterações finalizados

--- TabelasIdentity/obj/Debug/net6.0/TabelasIdentity.AssemblyInfo.cs
+++ TabelasIdentity/obj/Debug/net6.0/TabelasIdentity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TabelasIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+701cf965fc26749a4566ae743129121a468261c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TabelasIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TabelasIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
